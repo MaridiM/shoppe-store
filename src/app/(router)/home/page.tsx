4fc4394c4b709c6bb0adbe4c7dde835123ae395a
Dmitriy <dmytro.marynenko@scive.ai>
2024-08-11 22:51:30 +0300

    Add HomePage, home router

---
+++ src/app/(router)/home/page.tsx
@@ -0,0 +1,10 @@
+import { HomePage } from '@/pages'
+import React from 'react'
+
+const Home = () => {
+    return (
+        <HomePage />
+    )
+}
+
+export default Home
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react'
+
+interface IProps {}
+
+const HomePage: FC<IProps> = ({}) => {
+    return (
+        <div>
+            HOME PAGE APP
+        </div>
+    )
+}
+
+export default HomePage
