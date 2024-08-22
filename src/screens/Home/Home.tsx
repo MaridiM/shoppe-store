d536e86183b6719e10e64eb80dec85fdffbf1756
Dzmitry Arhei <demetris167@gmail.com>
2024-08-22 19:23:50 +0300

    add layout product card and products list

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -2,6 +2,7 @@
 
 import { HeartSVG, LogoSVG, SearchSVG, ShoppingCartSVG, UserSVG } from '@/shared/assets/icons';
 import gold_big_hoops from '@/shared/assets/img/slider/gold_big_hoops.jpg';
+import img_01 from '@/shared/assets/img/products/img_01.jpg';
 import { paths } from '@/shared/config/roures';
 import Link from 'next/link';
 import Image from 'next/image';
@@ -28,17 +29,17 @@ const HomePage: FC<IProps> = ({}) => {
         </div>
         <nav className="flex items-end gap-x-12">
           <ul className="flex items-end gap-x-16">
-            <li className="border-black border-b-2 py-[14px] text-h5">
+            <li className="border-black border-b-2 py-[14px] text-base">
               <Link href={paths.products()} className="text-black font-medium">
                 Shop
               </Link>
             </li>
-            <li className="border-transparent hover:border-black transition-all delay-150 ease-in-out border-b-2 py-[14px] text-h5">
+            <li className="border-transparent hover:border-black transition-all delay-150 ease-in-out border-b-2 py-[14px] text-base">
               <Link href={paths.posts()} className="text-black font-medium">
                 Blog
               </Link>
             </li>
-            <li className="border-transparent hover:border-black transition-all delay-150 ease-in-out border-b-2 py-[14px] text-h5">
+            <li className="border-transparent hover:border-black transition-all delay-150 ease-in-out border-b-2 py-[14px] text-base">
               <Link href={paths.about} className="text-black font-medium">
                 Our Story
               </Link>
@@ -71,15 +72,6 @@ const HomePage: FC<IProps> = ({}) => {
       </header>
 
       <Splide
-        onPaginationMounted={(splide: any) => {
-          const items = splide.Components.Pagination.items;
-          console.log(items);
-          items.forEach((item: any, index: any) => {
-            console.log(item.li);
-            // item.button.textContent = String(index + 1);
-          });
-        }}
-        ref={splideRef}
         tag="section"
         options={{
           type: 'loop',
@@ -92,36 +84,76 @@ const HomePage: FC<IProps> = ({}) => {
         aria-label="Homepage Slider"
       >
         <SplideSlide>
-          <Image className="rounded-md max-h-full object-cover object-top" src={gold_big_hoops} alt="img" />
+          <Image className="max-h-full object-cover object-top" src={gold_big_hoops} alt="img" />
           <div className="absolute top-56 left-10">
             <h1 className="mb-4 font-medium text-[33px] leading-[1.3] text-white">Gold big hoops</h1>
             <p className="mb-12 font-normal text-[26px] leading-[1.35] text-white">$ 68,00</p>
-            <button className="w-[193px] h-[53px] border-2 border-solid border-white text-white rounded-md text-h4 font-bold">
+            <button className="w-[193px] h-[53px] border-2 border-solid border-white text-white rounded-md text-xl font-bold">
               View Product
             </button>
           </div>
         </SplideSlide>
         <SplideSlide>
-          <Image className="rounded-md max-h-full object-cover object-top" src={gold_big_hoops} alt="img" />
+          <Image className="max-h-full object-cover object-top" src={gold_big_hoops} alt="img" />
           <div className="absolute top-56 left-10">
             <h1 className="mb-4 font-medium text-[33px] leading-[1.3] text-white">Gold big hoops</h1>
             <p className="mb-12 font-normal text-[26px] leading-[1.35] text-white">$ 68,00</p>
-            <button className="w-[193px] h-[53px] border-2 border-solid border-white text-white rounded-md text-h4 font-bold">
+            <button className="w-[193px] h-[53px] border-2 border-solid border-white text-white rounded-md text-xl font-bold">
               View Product
             </button>
           </div>
         </SplideSlide>
         <SplideSlide>
-          <Image className="rounded-md max-h-full object-cover object-top" src={gold_big_hoops} alt="img" />
+          <Image className="max-h-full object-cover object-top" src={gold_big_hoops} alt="img" />
           <div className="absolute top-56 left-10">
             <h1 className="mb-4 font-medium text-[33px] leading-[1.3] text-white">Gold big hoops</h1>
             <p className="mb-12 font-normal text-[26px] leading-[1.35] text-white">$ 68,00</p>
-            <button className="w-[193px] h-[53px] border-2 border-solid border-white text-white rounded-md text-h4 font-bold">
+            <button className="w-[193px] h-[53px] border-2 border-solid border-white text-white rounded-md text-xl font-bold">
               View Product
             </button>
           </div>
         </SplideSlide>
       </Splide>
+      <section className="mt-16">
+        <div className="flex justify-between items-end">
+          <h2 className="text-[33px] font-medium leading-[1.3]">Shop The Latest</h2>
+          <Link className="text-accent font-medium" href={'#'}>
+            View All
+          </Link>
+        </div>
+        <div className="flex justify-center flex-wrap mt-10 gap-y-[86px] gap-x-[57px]">
+          <div>
+            <Image className="rounded-lg" src={img_01} alt="product" />
+            <h2 className="mt-6 text-xl leading-[1.3]">Lira Earrings</h2>
+            <span className="inline-block text-xl mt-4 font-medium text-accent leading-[1.3] capitalize">$ 20,00</span>
+          </div>
+          <div>
+            <Image className="rounded-lg" src={img_01} alt="product" />
+            <h2 className="mt-6 text-xl leading-[1.3]">Lira Earrings</h2>
+            <span className="inline-block text-xl mt-4 font-medium text-accent leading-[1.3] capitalize">$ 20,00</span>
+          </div>
+          <div>
+            <Image className="rounded-lg" src={img_01} alt="product" />
+            <h2 className="mt-6 text-xl leading-[1.3]">Lira Earrings</h2>
+            <span className="inline-block text-xl mt-4 font-medium text-accent leading-[1.3] capitalize">$ 20,00</span>
+          </div>
+          <div>
+            <Image className="rounded-lg" src={img_01} alt="product" />
+            <h2 className="mt-6 text-xl leading-[1.3]">Lira Earrings</h2>
+            <span className="inline-block text-xl mt-4 font-medium text-accent leading-[1.3] capitalize">$ 20,00</span>
+          </div>
+          <div>
+            <Image className="rounded-lg" src={img_01} alt="product" />
+            <h2 className="mt-6 text-xl leading-[1.3]">Lira Earrings</h2>
+            <span className="inline-block text-xl mt-4 font-medium text-accent leading-[1.3] capitalize">$ 20,00</span>
+          </div>
+          <div>
+            <Image className="rounded-lg" src={img_01} alt="product" />
+            <h2 className="mt-6 text-xl leading-[1.3]">Lira Earrings</h2>
+            <span className="inline-block text-xl mt-4 font-medium text-accent leading-[1.3] capitalize">$ 20,00</span>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,57 +1,51 @@
-import type { Config } from "tailwindcss"
+import type { Config } from 'tailwindcss';
 
 const config = {
-  darkMode: ["class"],
-  content: [
-    './src/**/*.{ts,tsx}',
-	],
-  prefix: "",
+  darkMode: ['class'],
+  content: ['./src/**/*.{ts,tsx}'],
+  prefix: '',
   theme: {
     screens: {
-      'sm': '576px',
-      'md': '768px',
-      'lg': '960px',
-      'xl': '1280px',
+      sm: '576px',
+      md: '768px',
+      lg: '960px',
+      xl: '1280px',
       '2xl': '1536px',
     },
     extend: {
       fontSize: {
-        h1: '33px',
-        h2: '26px',
-        h3: '20px',
-        h4: '20px',
-        h5: '16px',
-        body:'14px',
-        'body-lg': '16px',
-        'body-sm': '12px',
+        sm: '14px',
+        base: '16px',
+        lg: '18px',
+        xl: '20px',
       },
       colors: {
-        background: 'hsl(var(--background))',
-        white: 'hsl(var(--white))',
-        black: 'hsl(var(--black))',
-        errors: 'hsl(var(--errors))',
-        accent: 'hsl(var(--accent))',
-        gray: 'hsl(var(--gray))',
-        'light-gray': 'hsl(var(--light-gray))',
-        'dark-gray': 'hsl(var(--dark-gray))',
+        background: 'var(--background)',
+        white: 'var(--white)',
+        black: 'var(--black)',
+        errors: 'var(--errors)',
+        accent: 'var(--accent)',
+        gray: 'var(--gray)',
+        'light-gray': 'var(--light-gray)',
+        'dark-gray': 'var(--dark-gray)',
       },
       keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
         },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
         },
       },
       animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
-} satisfies Config
+  plugins: [require('tailwindcss-animate')],
+} satisfies Config;
 
-export default config
+export default config;
