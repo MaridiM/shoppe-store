83640f85242e3f5451e03b012a30f71fc34ad7ca
HannaNikola <hanna.nikolaienko@scive.ai>
2024-09-04 22:15:01 +0200

    SHO-17Privace

--- src/screens/PrivacyPolisPage.tsx
+++ src/screens/PrivacyPolisPage.tsx
@@ -10,7 +10,7 @@ const PrivacyPolisPage: FC = () => {
     return (
         <Container className=' flex-col  px-[41px] py-[118px] sm:px-[144px] sm:pt-[118px] sm:pb-[96px] xl:px-[300px] xl:pt-[219px] xl:pb-[250px] '>
             <div className=''>
-                <h1 className='mb-[39px] text-center text-xl md:text-h1'>Privacy Policy</h1>
+                <h1 className='mb-[39px] md:text-center text-xl md:text-h1'>Privacy Policy</h1>
                 <p className='mb-[22px] md:mb-[39px] text-sm md:text-base'>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam placerat, augue
                     a volutpat hendrerit, sapien tortor faucibus augue, a maximus elit ex vitae
