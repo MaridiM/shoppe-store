4d5d82718bab8492dfd35ab2dbcb6ead5d8570bb
Dzmitry Arhei <demetris167@gmail.com>
2024-09-23 18:21:58 +0300

    fix layout

--- src/widgets/Footer/Footer.tsx
+++ src/widgets/Footer/Footer.tsx
@@ -11,9 +11,9 @@ interface IProps {}
 
 const Footer: FC<IProps> = () => {
     return (
-        <footer className='flex flex-col items-center h-auto lg:flex-row lg:items-end lg:h-40 justify-between border-t-2 border-t-light-gray'>
-            <div className='mt-12 lg:mt-0'>
-                <ul className='flex flex-col gap-y-3 justify-center items-center text-sm md:flex-row md:gap-x-10 lg:text-base'>
+        <footer className='flex flex-col h-auto justify-between border-t-2 border-t-light-gray lg:h-40'>
+            <div className='flex flex-col gap-y-10 sm:mt-12 sm:items-center lg:flex-row lg:justify-between'>
+                <ul className='flex flex-col order-1 gap-y-3 sm:items-center lg:justify-center text-sm lg:flex-row md:gap-x-10 lg:items-center lg:text-base lg:order-0'>
                     <li>
                         <Link href={paths.contact} className='leading-[1.69]'>
                             CONTACT
@@ -30,19 +30,7 @@ const Footer: FC<IProps> = () => {
                         </Link>
                     </li>
                 </ul>
-                <p className='mt-6 text-center md:mt-12 lg:text-left '>
-                    © 2021 Shelly.{' '}
-                    <Link href={paths.term} className='text-dark-gray'>
-                        Terms of use
-                    </Link>{' '}
-                    and
-                    <Link href={paths.term} className='text-dark-gray'>
-                        privacy policy.
-                    </Link>
-                </p>
-            </div>
-            <div className='mt-6 lg:mt-0 pb-[10px] w-full max-w-[396px]'>
-                <form className='relative w-full'>
+                <form className='relative order-0 w-full max-w-[396px] lg:order-1'>
                     <Input
                         className='pr-8 border-black leading-[1.69] placeholder:text-dark-gray'
                         name='email'
@@ -53,7 +41,12 @@ const Footer: FC<IProps> = () => {
                         <MoveRight size={25} />
                     </Button>
                 </form>
-                <ul className='flex justify-center lg:justify-end gap-x-[30px] mt-[50px]'>
+            </div>
+            <div className='flex relative flex-col lg:flex-row lg:justify-between pb-[10px]'>
+                <p className='absolute top-8 sm:hidden'>
+                    Follow us <span className='inline-block bg-black w-10 h-[1px] ml-1 mb-1'></span>
+                </p>
+                <ul className='flex gap-x-5 pl-32 mt-8 sm:gap-x-[30px] sm:pl-0 sm:justify-center lg:justify-end lg:mt-[50px]'>
                     <li>
                         <Link href='#'>
                             <LinkedinSVG
@@ -91,6 +84,16 @@ const Footer: FC<IProps> = () => {
                         </Link>
                     </li>
                 </ul>
+                <p className='text-xs mt-6 sm:text-center md:mt-12 sm:text-base lg:text-left '>
+                    © 2021 Shelly.{' '}
+                    <Link href={paths.term} className='text-dark-gray'>
+                        Terms of use
+                    </Link>{' '}
+                    and{' '}
+                    <Link href={paths.term} className='text-dark-gray'>
+                        privacy policy.
+                    </Link>
+                </p>
             </div>
         </footer>
     );
